b09a090f742ccb460b2ddc0b7ee7dba42bbb8220
Ayrton Avalos <ayrtonfidel9@gmail.com>
2021-05-19 11:27:24 -0500

    first commit cacao

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SolucionCacao.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
